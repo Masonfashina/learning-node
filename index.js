ba1edef334c0bddd75ecacf55b7d353119842e95
Mason <Masonfashina@icloud.com>
2023-12-28 21:51:00 +0100

    added my first REST api

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) =>{
     if (req.url === '/'){
-        res.end('<h1>Home</h1>')
+        fs.readFile(path.join(__dirname, 'public','index.html'), (err,content) =>{
+            if(err) throw err
+            res.writeHead(200, {'content-type': 'text/html'})
+            res.end(content);
+        })
     }
-})
+    if (req.url === '/api/users'){
+        const users =[
+            { name: 'Mason Fashina', age: 27  },
+            { name: 'Tamzyne Romans', age: 25  },
+        ]
+        res.writeHead(200, {'content-type': 'application/json'});
+        res.end(JSON.stringify(users))
+    }
+
+    }
+)
+
+
 
 
 const PORT = process.env.PORT || 5000;
